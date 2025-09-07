e72e2ca2a23ac8481ebcb1de4827113551154fe5
arkadeepg <112115235+arkadeepg@users.noreply.github.com>
2025-09-08 01:15:49 +0530

    DSA
    change lists as arrays

---
+++ 02-Arrays/01_array.py
@@ -0,0 +1,48 @@
+# List (Array), Dynamic Array in Python
+num = [123, 432, "strjk", 453]
+
+#print list
+print(num)
+print(num[0])
+
+#length of list
+print(len(num))
+
+#add element to list
+num.append(876)
+print(num)
+
+#remove element from list
+num.remove(876)
+print(num)
+
+#insert after specific element/ location
+if "strjk" in num:
+    for idx, val in enumerate(num):
+        if val == "strjk":
+            num.insert(idx+1, 876)
+            break
+print(num)
+
+#replace elements at index positions 1 and 2 in the list num with the single value 600
+num[1:3] = [600]
+print(num)
+
+#sort the list
+num.sort()
+print(num)
+
+
+##################################################
+
+
+print("\n")
+
+# List of Dictionaries
+expenses = [{"Month":"January", "Expense":2200},
+                  {"Month":"February", "Expense": 2350},
+                  {"Month":"March", "Expense": 2600},
+                  {"Month":"April", "Expense": 2130},
+                  {"Month":"May", "Expense": 2190}]
+
+print(expenses[1]["Expense"])
